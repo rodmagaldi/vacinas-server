6750f06ca93128fbf501dccce7f94660f9a5a443
rodmagaldi <rod.magaldi@gmail.com>
2021-06-15 22:10:36 -0300

    Update user mock

--- src/test/mock/user.mock.ts
+++ src/test/mock/user.mock.ts
@@ -1,7 +1,8 @@
 import * as cpf from 'cpf';
-import { name, internet } from 'faker';
+import { name, internet, phone } from 'faker';
 import { User } from '@server/data/db/entity';
 import { getRepository } from 'typeorm';
+import { RaceOptions, GenderOptions } from '@server/domain/model';
 
 export async function mockUsers(numberOfUsers: number): Promise<void> {
   const usersRepository = getRepository(User);
@@ -14,6 +15,11 @@ export async function mockUsers(numberOfUsers: number): Promise<void> {
       lastName: name.lastName(),
       email: internet.email(),
       cpf: cpf.generate(),
+      cns: cpf.generate(),
+      phone: phone.phoneNumber(),
+      race: RaceOptions.branco,
+      gender: GenderOptions.masculino,
+      motherName: name.firstName(),
     };
     const newUser = usersRepository.create(user);
     usersList.push(newUser);
